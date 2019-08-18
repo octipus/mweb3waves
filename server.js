d86f9d12fa7c4e919ea7dc52751847e14979ae61
Octavian Contis <bmx_tavi@yahoo.com>
2019-08-18 14:42:05 +0100

    Mastering Web3 With waves 1.1

---
+++ server.js
@@ -0,0 +1,15 @@
+//send the index.html file to the browser once the user has opened the web-site page.
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.sendFile('index.html', { root : __dirname});
+});
+
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(__dirname);
+	console.log("Listening Port " + port);
+});
